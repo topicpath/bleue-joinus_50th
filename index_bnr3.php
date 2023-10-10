08eaa7ad2072b0d3bd0a679c92ca075dd90fc8d9
小川保 <ogawa@tsukurite.com>
2023-10-11 03:38:38 +0900

    バナー3種

---
+++ index_bnr3.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
+<meta charset="UTF-8">
+<!-- Google tag (gtag.js) -->
+<script async src=" https://www.googletagmanager.com/gtag/js?id=G-EFY7FD6VT5 "></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-EFY7FD6VT5');
+</script>
+
+<title>JOINUS 50th | ジョイナス50周年</title>
+<meta name="description" content="いつもと、未来へ。おかげさまで、JOINUSは今年50周年を迎えます。つぎの50年も、みなさんのいつもの存在であり続けるために。みなさんのキモチに応えていくために。いつもを積み重ね、より良い未来へともに進んでいく。あたらしいチャレンジを続けていく。これからもJOINUSにどうぞご期待ください。">
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<meta name="format-detection" content="telephone=no">
+<script>
+var ua = navigator.userAgent.toLowerCase();
+if ((ua.indexOf('iphone') != -1) || ua.indexOf('ipod') != -1 || (ua.indexOf("android") != -1 && ua.indexOf("mobile") != -1) || (ua.indexOf("firefox") != -1 && ua.indexOf("mobile") != -1) || (ua.indexOf("windows") != -1 && ua.indexOf("phone") != -1)) {
+//	document.write('<meta name="viewport" content="width=1300">');
+}
+else if(ua.indexOf('ipad') > -1 || (ua.indexOf('macintosh') > -1 && 'ontouchend' in document) || ua.indexOf('android') > -1){
+//	document.write('<meta name="viewport" content="width=1300">');
+}
+</script>
+
+<!-- font -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500;600&display=swap" rel="stylesheet">
+<!-- / font -->
+
+<!-- css -->
+<link href="t/css/50th.css" rel="stylesheet">
+<!-- / css -->
+
+<!-- script -->
+<script src="t/js/jquery-3.7.0.min.js" defer></script>
+<script src="t/js/50th.js" defer></script>
+<!-- / script -->
+
+<link rel="canonical" href="https://www.sotetsu-joinus.com/50th/">
+
+<!-- ogp -->
+<meta property="og:title" content="JOINUS 50th | ジョイナス50周年">
+<meta property="og:type" content="article">
+<meta property="og:url" content="https://www.sotetsu-joinus.com/50th/">
+<meta property="og:image" content="https://www.sotetsu-joinus.com/50th/ogp.png">
+<meta property="og:image:width" content="1200">
+<meta property="og:image:height" content="630">
+<meta property="og:site_name" content="JOINUS | ジョイナス">
+<meta property="og:description" content="いつもと、未来へ。おかげさまで、JOINUSは今年50周年を迎えます。つぎの50年も、みなさんのいつもの存在であり続けるために。みなさんのキモチに応えていくために。いつもを積み重ね、より良い未来へともに進んでいく。あたらしいチャレンジを続けていく。これからもJOINUSにどうぞご期待ください。">
+<meta name="twitter:card" content="summary_large_image">
+<!-- / ogp -->
+
+<link rel="icon" href="https://www.sotetsu-joinus.com/sites/default/files/cropped-favicon-192x192.png" type="image/png">
+
+</head>
+
+<body>
+<div id="main">
+	<h1 id="logo"><img src="t/img/logo-50th.svg" alt="JOINUS 50th" width="384" height="280"></h1>
+	<div id="j">
+<?php for($i = 1; $i <= 50; $i ++) : ?>
+		<div class="j-<?php echo sprintf('%02d', $i); ?>"><img src="t/img/j-<?php echo sprintf('%02d', $i); ?>.webp" alt="" width="260" height="320"></div>
+<?php endfor; ?>
+	</div>
+
+	<div class="swipe_notes">
+		<div class="arrow">
+			<picture>
+				<source media="screen and (max-width: 767px)" srcset="t/img/ico-arrow-sp.svg">
+				<img src="t/img/ico-arrow-pc.svg" alt="" width="380" height="70">
+			</picture>
+		</div>
+		<p class="pc">左から右へマウスを動かし火を吹き消してください</p>
+		<p class="sp">左から右へ<span>スワイプして</span><br>火を吹き消してください</p>
+	</div><!-- /.swipe_notes -->
+</div><!-- /#main -->
+
+<div id="contents">
+	<div class="logo-header"><a href="https://www.sotetsu-joinus.com/" target="_blank"><img src="https://www.sotetsu-joinus.com/img/top/logo.svg" alt="相鉄ジョイナス" width="232" height="97"></a></div>
+	<div class="cover"></div>
+	<div class="text">
+		<picture>
+			<source media="screen and (max-width: 767px)" srcset="t/img/text-sp.png">
+			<img src="t/img/text-pc.svg" alt="いつもと、未来へ。おかげさまで、JOINUSは今年50周年を迎えます。つぎの50年も、みなさんのいつもの存在であり続けるために。みなさんのキモチに応えていくために。いつもを積み重ね、より良い未来へともに進んでいく。あたらしいチャレンジを続けていく。これからもJOINUSにどうぞご期待ください。" width="1300" height="810">
+		</picture>
+
+		<img src="t/img/j-06.webp" alt="" width="260" height="320" class="j j-01">
+		<img src="t/img/j-16.webp" alt="" width="260" height="320" class="j j-02">
+		<img src="t/img/j-28.webp" alt="" width="260" height="320" class="j j-03">
+		<img src="t/img/j-35.webp" alt="" width="260" height="320" class="j j-04">
+		<img src="t/img/j-15.webp" alt="" width="260" height="320" class="j j-05">
+		<img src="t/img/j-26.webp" alt="" width="260" height="320" class="j j-06">
+		<img src="t/img/j-25.webp" alt="" width="260" height="320" class="j j-07">
+		<img src="t/img/j-36.webp" alt="" width="260" height="320" class="j j-08">
+		<img src="t/img/j-24.webp" alt="" width="260" height="320" class="j j-09">
+	</div><!-- /.text -->
+
+	<p class="event">
+		<picture>
+			<source media="screen and (max-width: 767px)" srcset="t/img/event-sp.svg">
+			<img src="t/img/event-pc.svg" alt="11月には感謝を込めて、50周年を記念した限定アイテムや特典サービス、イベントが開催されます。" width="834" height="126">
+		</picture>
+	</p><!-- /.event -->
+
+
+	<footer id="footer">
+		<p class="logo"><a href="https://www.sotetsu-joinus.com/" target="_blank"><img src="t/img/logo-joinus.svg" alt="JOINUS YOKOHAMA" width="130" height="56"></a></p>
+		<p class="address">〒220-0005 神奈川県横浜市西区南幸1-5-1<br>TEL：<a href="tel:045-316-3200">045-316-3200</a></p>
+		<small>Copyright©2023 JOINUS All Rights Reserved.</small>
+	</footer><!-- /#footer -->
+</div><!-- /#contents -->
+
+<div id="bnr">
+	<ul>
+		<li><a href="#" target="_blank"><picture>
+			<source media="screen and (max-width: 767px)" srcset="t/img/bnr01_sp.png">
+			<img src="t/img/bnr01.png" alt="コート下取り2DAYS 10.28 SAT. 29 SUN" width="640" height="240">
+		</picture></a></li>
+		<li><a href="premium-okaimono/"><picture>
+			<source media="screen and (max-width: 767px)" srcset="t/img/bnr02_sp.png">
+			<img src="t/img/bnr02.png" alt="プレミアムお買物券 11.2 THU 朝10時予約スタート!!" width="640" height="240">
+		</picture></a></li>
+		<li><a href="items-service/"><picture>
+			<source media="screen and (max-width: 767px)" srcset="t/img/bnr03_sp.png">
+			<img src="t/img/bnr03.png" alt="ジョイナス50周年記念 限定アイテム＆サービス 11.1WED スタート" width="640" height="240">
+		</picture></a></li>
+	</ul>
+</div><!-- /#bnr -->
+
+</body>
+</html>
