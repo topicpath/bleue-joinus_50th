6e5fbc5f63cfa67a73fec523041a811d5ea6b4fe
小川保 <ogawa@tsukurite.com>
2023-10-19 18:59:17 +0900

    gtag

--- include/tag-head_start.php
+++ include/tag-head_start.php
@@ -1,3 +1,9 @@
-<?php
-// Googleタグマネージャタグ等
-?>
+<!-- Google tag (gtag.js) -->
+<script async src=" https://www.googletagmanager.com/gtag/js?id=G-EFY7FD6VT5"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-EFY7FD6VT5');
+</script>
