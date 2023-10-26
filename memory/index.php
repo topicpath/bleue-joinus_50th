c74937bbbff6c8bfb72abdadfb412ac4f5251ff1
小川保 <ogawa@tsukurite.com>
2023-10-26 22:08:20 +0900

    思い出修正

--- memory/index.php
+++ memory/index.php
@@ -37,7 +37,8 @@ include_once 'common-meta.php';
 		<div class="memory_wrapper">
 			<header class="memory_header mov_header mov_first">
 				<h1><span><img src="images/logo.svg" alt="#JOINUS" width="198" height="34">の思い出</span><span>教えてください</span></h1>
-				<p>「相鉄 公式」Twitterアカウントで実施した<br>「#JOINUSの思い出教えてください」<br class="sp">投稿募集企画に、<br>おかげさまで多くの思い出が寄せられました。<br>そんな皆さまの思い出を紹介します。</p>
+				<p>「相鉄 公式」X（旧Twitter）に寄せられた<br>皆様からの思い出を当時の写真とともに紹介します。</p>
+				<p class="s">（「#JOINUSの思い出教えてください」2023年7月14日～23日実施 ※一部抜粋。）</p>
 			</header><!-- /.memory_header -->
 
 			<div class="memory_history_ph mov_show fadeup">
@@ -45,43 +46,43 @@ include_once 'common-meta.php';
 					<div class="swiper-wrapper">
 						<figure class="swiper-slide">
 							<p class="ph"><img src="images/slide_ph01.jpg" alt="ジョイナスオープン" width="570" height="470"></p>
-							<figcaption>1973年11月<br class="sp"> ジョイナスオープン<br>（相鉄ビルから、気球とジョイナス）</figcaption>
+							<figcaption><strong>1973年11月</strong> 相鉄ジョイナス開業</figcaption>
 						</figure>
 						<figure class="swiper-slide">
 							<p class="ph"><img src="images/slide_ph02.jpg" alt="ジョイナスオープニングイベント" width="570" height="470"></p>
-							<figcaption>1973年11月<br class="sp"> ジョイナスオープニングイベント<br>（西口、風船を持つ女性達）</figcaption>
+							<figcaption><strong>1973年11月</strong> 相鉄ジョイナス開業</figcaption>
 						</figure>
 						<figure class="swiper-slide">
 							<p class="ph"><img src="images/slide_ph03.jpg" alt="ジョイナス1周年記念" width="570" height="470"></p>
-							<figcaption>1974年11月<br class="sp"> ジョイナス1周年記念<br>（自然の広場フォークソングの集い俯瞰）</figcaption>
+							<figcaption><strong>1974年11月</strong> 相鉄ジョイナス1周年記念イベント<br>（4階にあった自然の広場）</figcaption>
 						</figure>
 						<figure class="swiper-slide">
 							<p class="ph"><img src="images/slide_ph04.jpg" alt="ジョイナス1階24時間通路 秋のジョイナス" width="570" height="470"></p>
-							<figcaption>1977年<br class="sp"> ジョイナス1階24時間通路<br>秋のジョイナス</figcaption>
+							<figcaption><strong>1977年</strong> ジョイナス1階大通り</figcaption>
 						</figure>
 						<figure class="swiper-slide">
 							<p class="ph"><img src="images/slide_ph05.jpg" alt="ジョイナスの森" width="570" height="470"></p>
-							<figcaption>1978年11月<br class="sp"> ジョイナスの森</figcaption>
+							<figcaption><strong>1978年11月</strong> 屋上・ジョイナスの森<br>（彫刻公園になる以前）</figcaption>
 						</figure>
 						<figure class="swiper-slide">
 							<p class="ph"><img src="images/slide_ph06.jpg" alt="ジョイナス地下・噴水" width="570" height="470"></p>
-							<figcaption>1982年9月<br class="sp"> ジョイナス地下・噴水</figcaption>
+							<figcaption><strong>1982年9月</strong> 地下2階にあった泉の広場</figcaption>
 						</figure>
 						<figure class="swiper-slide">
 							<p class="ph"><img src="images/slide_ph07.jpg" alt="ジョイナスイレブン入口（正面）" width="570" height="470"></p>
-							<figcaption>1983年6月<br class="sp"> ジョイナスイレブン入口<br class="sp">（正面）</figcaption>
+							<figcaption><strong>1983年6月</strong> 地下1階にあった<br>「ジョイナスイレブン」</figcaption>
 						</figure>
 						<figure class="swiper-slide">
 							<p class="ph"><img src="images/slide_ph08.jpg" alt="ジョイナスの森彫刻公園しゅん工" width="570" height="470"></p>
-							<figcaption>1983年11月<br class="sp"> ジョイナスの森彫刻公園しゅん工<br>（10周年）</figcaption>
+							<figcaption><strong>1983年11月</strong> 屋上・ジョイナスの森彫刻公園オープン</figcaption>
 						</figure>
 						<figure class="swiper-slide">
 							<p class="ph"><img src="images/slide_ph09.jpg" alt="ジョイナス自然の広場" width="570" height="470"></p>
-							<figcaption>1987年7月<br class="sp"> ジョイナス自然の広場</figcaption>
+							<figcaption><strong>1987年7月</strong> 4階にあった自然の広場</figcaption>
 						</figure>
 						<figure class="swiper-slide">
 							<p class="ph"><img src="images/slide_ph10.jpg" alt="ジョイナスグランドオープン" width="570" height="470"></p>
-							<figcaption>2015年12月<br class="sp"> ジョイナスグランドオープン<br>（地下街メイン通路）</figcaption>
+							<figcaption><strong>2015年12月</strong> 新生ジョイナスグランドオープン</figcaption>
 						</figure>
 					</div>
 					<a class="arrow swiper-button-prev"></a>
@@ -114,7 +115,7 @@ foreach ($datas as $item) : ?>
 
 		</div><!-- /.memory_wrapper -->
 
-		<p class="bottom_notes w sp_pd">※投稿いただいた思い出の一部を掲載しています。<br>※投稿内容については、一部加筆修正しています。</p>
+		<p class="bottom_notes w sp_pd">※投稿いただいた思い出の一部を掲載しています。<br>※投稿内容については、一部加筆修正しています。<br>※順不同</p>
 
 
 		<a class="fix_message_btn"><span><span class="en">JOINUS50</span>周年<br>メッセージを見る</span></a>
