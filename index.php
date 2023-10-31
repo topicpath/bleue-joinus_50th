e3970b1feaa45a68a293202d318daa11f4b8d265
小川保 <ogawa@tsukurite.com>
2023-10-31 13:37:04 +0900

    11/2 プレミアムお買い物販売開始時

--- index.php
+++ index.php
@@ -48,6 +48,10 @@ include_once 'common-meta.php';
 		</header>
 		<div>
 			<ul class="topics">
+				<li><a href="premium-okaimono/">
+					<span class="time ff_en">2023.11.2</span>
+					<span class="title">「プレミアムお買物券」事前予約受付を開始いたしました。</span>
+				</li></a>
 				<li><a href="https://www.sotetsu-joinus.com/news/4329" target="_blank">
 					<span class="time ff_en">2023.11.1</span>
 					<span class="title">ジョイナス50周年特別企画！ジョイナスポイント5倍!!11月22日（水）～11月26日（日）実施決定！</span>
@@ -56,13 +60,13 @@ include_once 'common-meta.php';
 					<span class="time ff_en">2023.11.1</span>
 					<span class="title">ジョイナスポイント会員限定！「プレミアムお買物券」販売の事前予約が11月2日（木）朝10時スタート！</span>
 				</li></a>
-				<li><a href="https://www.sotetsu-joinus.com/news/4328" target="_blank">
-					<span class="time ff_en">2023.11.1</span>
-					<span class="title">Anniversary ワークショップ「廃材を使って作ろう！世界にひとつだけのJちゃん」を11月3日（金）4日（土）開催。</span>
-				</li></a>
 			</ul>
 			<div class="more_topics">
 				<ul class="topics">
+					<li><a href="https://www.sotetsu-joinus.com/news/4328" target="_blank">
+						<span class="time ff_en">2023.11.1</span>
+						<span class="title">Anniversary ワークショップ「廃材を使って作ろう！世界にひとつだけのJちゃん」を11月3日（金）4日（土）開催。</span>
+					</li></a>
 					<li><a href="https://www.sotetsu-joinus.com/news/4330" target="_blank">
 						<span class="time ff_en">2023.11.1</span>
 						<span class="title">11月19日（日）に開催するSDGsワークショップ「さつまいも掘りと植樹体験」の参加者募集開始。</span>
